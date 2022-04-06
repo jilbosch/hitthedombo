5dde09aec08a58ea69c31c90be30c1fcd5c610cf
EudaldFerrer <eudald.ferrer@gmail.com>
2022-04-06 13:33:39 +0200

    Foto+Botons+JavaScript

---
+++ pagina-principal/app.js
@@ -0,0 +1,23 @@
+var bombo = new Audio();
+bombo.src = "../sounds-hit-Domboom/bombo (1).wav";
+
+var crash = new Audio();
+crash.src = "../sounds-hit-Domboom/crash.wav";
+
+var hit = new Audio();
+hit.src = "../sounds-hit-Domboom/hit-hat.wav";
+
+var redoblante = new Audio();
+redoblante.src = "../sounds-hit-Domboom/redoblante.wav";
+
+var tom1 = new Audio();
+tom1.src = "../sounds-hit-Domboom/tom1.wav";
+
+var tom2 = new Audio();
+tom2.src = "../sounds-hit-Domboom/tom2.wav";
+
+var tom3 = new Audio();
+tom3.src = "../sounds-hit-Domboom/tom3.wav";
+
+var tom4 = new Audio();
+tom4.src = "../sounds-hit-Domboom/tom4.wav";
